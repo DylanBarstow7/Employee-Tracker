ce1f056e9951c9adb217078c1062c66cb5f5f1d9
Dylan Barstow <dcbarstow7@gmail.com>
2021-09-29 17:08:38 -0500

    all departments query

--- db/index.js
+++ db/index.js
@@ -5,5 +5,15 @@ class Database {
     this.connection = connection;
 	}
 
+	showAllDepartments() {
+		return this.connection.promise().query(
+			'SELECT departments.id, departments.dept_name FROM departments;'
+			);
+	}
+
+
+
+
+	
 
 module.exports = new Database(connection);
