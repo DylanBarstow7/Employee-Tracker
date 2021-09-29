623b261a9026cf6852b997eef1b0856a385004a3
Dylan Barstow <dcbarstow7@gmail.com>
2021-09-29 15:39:47 -0500

    created switch cases for roles

--- index.js
+++ index.js
@@ -1,18 +1,44 @@
 import inquirer from 'inquirer';
 import db from "./db";
 
-// this function will hold the content displayed on screen
+// creates list of options for the first step of application
 function renderOptionList() {
     inquirer.prompt(
         [{
             type: "list",
-            name: "option",
+            name: "options",
             message: "Please select from the following:/n",
-            Options: ["view all departments", "view all roles", "view all employees", "add a department", "add a role", "add an employee", "update an employee role"],
+            choices: ["view all departments", "view all roles", "view all employees", "add a department", "add a role", "add an employee", "update an employee role"],
         },
         ]
     )
-
+    .then((response) =>
+    {
+        switch(response.options) {
+        case "view all departments":
+            viewAllDepartments();
+            break;
+        case "view all roles":
+            viewAllRoles();
+            break;
+        case "view all employees":
+            viewAllEmployees();
+            break;
+        case "add a department":
+            addDepartment();
+            break;
+        case "add a role":
+            addRole();
+            break;
+        case "add an employee":
+            addEmployee();
+            break;
+        case "update an employee role":
+            addEmployeeRole();
+            break;
+        }
+        
+        )
 
 
 }
