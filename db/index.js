a329ca6b495c299e721aa322ce2e016e7d3e3b3f
Dylan Barstow <dcbarstow7@gmail.com>
2021-09-29 23:19:01 -0500

    viewership

--- db/index.js
+++ db/index.js
@@ -4,41 +4,54 @@ class Database {
   constructor(connection) {
     this.connection = connection;
 	}
-	
-// This calls the index.js f
-	findAllDeps() {
-		console.log("Hello");
+	findAllDeps(){
 		return this.connection.query(`
-			SELECT department.department_id, department.depName FROM department ORDER BY department.id ASC;`);
+			SELECT
+			department_id, department.depName
+			FROM
+			department;`)
 		}
-
 	findAllRoles() {
 		return this.connection.query(`
-			SELECT roles.title, roles.id, department.depName, roles.salary FROM roles JOIN department ON roles.department_id = department.id ORDER BY roles.id ASC;`);
+			SELECT roles.title, roles.id, department.depName, roles.salary
+			FROM roles
+			JOIN department ON roles.department_id = department.id
+			ORDER BY
+			roles.id ASC;`)
 	}
 
 	// findAllManagers(employeeId) {
 
 	// }
 
-	findAllEmp() {
+	findAllEmps() {
 		return this.connection.query(`
-			SELECT employee.id, employee.first_name, employee.last_name, roles.title, department.name AS department, roles.salary, CONCAT(manager.first_name," ", manager.last_name) ORDER BY employee.id ASC;`);
+			SELECTemployee.id, employee.first_name, employee.last_name, roles.title, department.depName, roles.salary, employee.manager_id
+			FROM employee
+			JOIN roles ON employee.role_id = roles.id
+			JOIN department ON roles.department_id = departments.id
+			ORDER BY
+			employee.id ASC;`)
 	}
 
-	createDep(newDep) {
-		return this.connection.query(`INSERT INTO department(dep_name) VALUES (?)`, newDep);
+	queryCreateDep(newDep) {
+		return this.connection.query(`
+		INSERT INTO department(depName)
+		VALUES (?)`, newDep)
 	}
 
 	createRole(roles) {
-		return this.connection.promise().query(`INSERT INTO role SET ?`, roles);
+		return this.connection.query(`
+		INSERT INTO roles
+		SET ?`, roles);
 	}
 
 	updateEmpRole(empId, roleId) {
-		return this.connection.promise().query(`
-			UPDATE employee SET role_id = ? WHERE id = ?;`,
-			[empId, roleId]
-		)
+		return this.connection.query(`
+			UPDATE employee 
+			SET role_id = ? 
+			WHERE id = ?;`,
+			[empId, roleId])
 	}
 }
 module.exports = new Database(connection);
