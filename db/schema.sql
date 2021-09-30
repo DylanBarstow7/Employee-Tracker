7bdf4223df9037d095f0e4eae62fb093f9b95fbd
Dylan Barstow <dcbarstow7@gmail.com>
2021-09-29 23:32:38 -0500

    schema update

--- db/schema.sql
+++ db/schema.sql
@@ -4,27 +4,30 @@ CREATE DATABASE employees;
 USE employees;
 
 CREATE TABLE department (
-	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-	depName VARCHAR(30) UNIQUE NOT NULL
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  depName VARCHAR(30) UNIQUE NOT NULL
 );
 
 CREATE TABLE roles (
-  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-  title VARCHAR(30) UNIQUE NOT NULL,
-  salary DECIMAL UNSIGNED NOT NULL,
-  department_id INT UNSIGNED NOT NULL,
-  INDEX dep_ind (department_id),
-  CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL NOT NULL,
+  department_id INT NOT NULL,
+  INDEX department_index (department_id),
+  CONSTRAINT fk_department_id FOREIGN KEY (department_id)
+  REFERENCES department(id)
 );
 
 CREATE TABLE employee (
-  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
-  role_id INT UNSIGNED NOT NULL,
-  INDEX role_ind (role_id),
-  CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
-  manager_id INT UNSIGNED,
-  INDEX man_ind (manager_id),
-  CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
+  role_id INT NOT NULL,
+  INDEX role_index (role_id),
+  CONSTRAINT fk_role_id FOREIGN KEY (role_id)
+  REFERENCES roles(id),
+  manager_id INT,
+  INDEX manager_index (manager_id),
+  CONSTRAINT fk_manager_id FOREIGN KEY (manager_id)
+  REFERENCES employee(id)
 );
