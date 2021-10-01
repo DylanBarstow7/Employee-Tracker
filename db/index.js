53d1ecada8aa32d8221ebd350d01a382b2f647b3
Dylan Barstow <dcbarstow7@gmail.com>
2021-10-01 01:56:23 -0500

    working tables!!!

--- db/index.js
+++ db/index.js
@@ -12,7 +12,7 @@ class Database {
 			SELECT
 			department.id, department.depName
 			FROM
-			department;`)
+			department;`);
 		}
 	queryAllRoles() {
 		return this.connection.promise().query(`
@@ -20,9 +20,8 @@ class Database {
 			FROM roles
 			JOIN department ON roles.department_id = department.id
 			ORDER BY
-			roles.id ASC;`)
+			roles.id ASC;`);
 	}
-
 	queryAllEmps() {
 		return this.connection.promise().query(`
 			SELECT employee.id, employee.first_name, employee.last_name, roles.title, department.depName, roles.salary, employee.manager_id
@@ -30,34 +29,32 @@ class Database {
 			JOIN roles ON employee.role_id = roles.id
 			JOIN department ON roles.department_id = department.id
 			ORDER BY
-			employee.id ASC;`)
+			employee.id ASC;`);
 	}
 
-	createDep(newDep) {
+	createDep(department) {
 		return this.connection.promise().query(`
-		INSERT INTO department(depName)
-		VALUES (?)`,newDep)
+		INSERT INTO department SET ?`, department);
 	}
 
 	createRole(title,salary,depId) {
 		return this.connection.promise().query(`
 		INSERT INTO roles(title,salary,department_id)
-		VALUES (?,?,?)`,[title,salary,depId])
+		VALUES (?,?,?)`,[title,salary,depId]);
 	}
 
 	
-	queryNewEmp(first,last,role,manager){
+	queryNewEmp(employee){
 		return this.connection.promise().query(`
-		INSERT INTO employee(first_name,last_name,department_id)
-		VALUES (?,?,?,?)`,[first,last,role,manager])
+		INSERT INTO employee SET ?`, employee);
 	}
 
-	queryUpdRole(roleId,empId) {
+	queryUpdRole(empId,roleId) {
 		return this.connection.promise().query(`
 			UPDATE employee 
 			SET role_id = ? 
-			WHERE id = ?;`,
-			[roleId,empId])
+			WHERE id = ?`,
+			[roleId,empId]);
 	}
 }
 module.exports = new Database(connection);
