acc8aabe749ba2c9e730cfe5fc798547099bdabf
Dylan Barstow <dcbarstow7@gmail.com>
2021-10-01 00:20:17 -0500

    updated naming

--- db/index.js
+++ db/index.js
@@ -1,17 +1,19 @@
 const connection = require('./connection');
 
+// creating general Database class
 class Database {
-  constructor(connection) {
+	constructor(connection) {
     this.connection = connection;
 	}
-	findAllDeps(){
+	queryAllDeps(){
+		console.log("hello");
 		return this.connection.query(`
 			SELECT
 			department.id, department.depName
 			FROM
 			department;`)
 		}
-	findAllRoles() {
+	queryAllRoles() {
 		return this.connection.query(`
 			SELECT roles.title, roles.id, department.depName, roles.department_id, roles.salary
 			FROM roles
@@ -20,12 +22,12 @@ class Database {
 			roles.id ASC;`)
 	}
 
-	findAllEmps() {
+	queryAllEmps() {
 		return this.connection.query(`
-			SELECTemployee.id, employee.first_name, employee.last_name, roles.title, department.depName, roles.salary, employee.manager_id
+			SELECT employee.id, employee.first_name, employee.last_name, roles.title, department.depName, roles.salary, employee.manager_id
 			FROM employee
 			JOIN roles ON employee.role_id = roles.id
-			JOIN department ON roles.department_id = departments.id
+			JOIN department ON roles.department_id = department.id
 			ORDER BY
 			employee.id ASC;`)
 	}
@@ -43,13 +45,13 @@ class Database {
 	}
 
 	
-	createEmp(first,last,role,manager){
+	queryNewEmp(first,last,role,manager){
 		return this.connection.query(`
 		INSERT INTO employee(first_name,last_name,department_id)
 		VALUES (?,?,?,?)`,[first,last,role,manager])
 	}
 
-	updateEmpRole(roleId,empId) {
+	queryUpdRole(roleId,empId) {
 		return this.connection.query(`
 			UPDATE employee 
 			SET role_id = ? 
